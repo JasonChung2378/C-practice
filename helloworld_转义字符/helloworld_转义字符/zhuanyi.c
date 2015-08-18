41e5647041423912cf8611fbe254b7a572cdf0b2
JasonChung2378 <2108277306@qq.com>
2015-08-18 09:09:59 +0800

    ...
    ......

---
+++ helloworld_转义字符/helloworld_转义字符/zhuanyi.c
@@ -0,0 +1,31 @@
+//#include<stdio.h>
+//#include<windows.h>
+//#include<stdlib.h>
+//void main(){
+//
+//	while(1){
+//	
+//	    malloc(1000000) ;
+//
+//		Sleep(1000); //µ»¥˝“ª√Î,1000∫¡√Î
+//	
+//	}
+//
+//
+//
+//}
+
+//int main(){
+//
+//
+//	while(1){
+//
+//	printf( "helloworld\a\n" ) ;
+//
+//	}
+//
+//	getchar( );
+//
+//
+//
+//}
---
+++ shutdown/shutdown/shutdown.cpp
@@ -0,0 +1,8 @@
+#include<windows.h>
+
+void main(){
+
+	MessageBoxA(0, "ÍøÂç¹ÊÕÏ£¬ÇëÖØĞÂµÇÂ¼", "QQ¹ÊÕÏ", 1) ;
+
+
+}
